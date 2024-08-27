cf33c11639cd6ea36fc44c72dc1952841155da42
zhephyn <zhephynwinter@gmail.com>
2024-08-27 18:32:43 +0300

    created a post model and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,46 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Successfully created post"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Successfully updated post"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Successfully deleted post"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
