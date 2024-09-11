da3330e6fc2cdab963552a94d762a013ead10377
zhephyn <zhephynwinter@gmail.com>
2024-09-12 01:34:14 +0300

    Added Background Jobs for broadcasting with the broadcasts_to method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
 class Post < ApplicationRecord
-    after_create_commit -> {broadcast_prepend_to "posts", partial: "posts/post", locals: { post: self }, target: "posts"}
-    after_update_commit -> {broadcast_update_to "posts", partial: "posts/post", locals: {post: self}, target: self}
-    after_destroy_commit -> {broadcast_remove_to "posts", target: self}
+    broadcasts_to ->(post) {:posts}, inserts_by: :prepend, target: "posts"
 end
