0fca673bd9019f967e94f1d50b2ff7f59a3ba3fc
zhephyn <zhephynwinter@gmail.com>
2024-09-24 12:22:00 +0300

    added tests for turbo stream actions

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,18 +1,39 @@
+
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get posts_index_url
-    assert_response :success
+  setup do
+    @post = posts(:one)
   end
 
-  test "should get new" do
-    get posts_new_url
+  test "should create post" do
+    assert_difference("Post.count", 1) do
+      post posts_url, params: { post: { body: "New post content" } }, as: :turbo_stream
+    end
+
     assert_response :success
+    assert_turbo_stream action: :prepend, target: "posts"
+    assert_turbo_stream action: :update, target: "flash"
+    assert_match /New post content/, response.body
+    assert_match /Post was successfully created/, response.body
   end
 
-  test "should get show" do
-    get posts_show_url
+  test "should update post" do
+    patch post_url(@post), params: { post: { body: "Updated post content" } }, as: :turbo_stream
+
     assert_response :success
+    assert_turbo_stream action: :update, target: "flash"
+    assert_match /Post was successfully updated/, response.body
+  end
+
+  test "should destroy post" do
+    assert_difference("Post.count", -1) do
+      delete post_url(@post), as: :turbo_stream
+    end
+
+    assert_response :success
+    assert_turbo_stream action: :remove, target: @post
+    assert_turbo_stream action: :update, target: "flash"
+    assert_match /Post was successfully deleted/, response.body
   end
 end
